b04baa69529574870c9bb5e1a044ef29b061821e
mohits-git <mohitkhatri2805@gmail.com>
2024-07-16 09:29:17 +0530

    feat: add main.py and TextNode class to represent inline texts

---
+++ src/main.py
@@ -0,0 +1,12 @@
+from textnode import TextNode
+
+
+def main():
+    node1 = TextNode("This is a text node", "bold", "https://www.boot.dev")
+    print(node1.__repr__())
+    node2 = TextNode("Hello", "italic")
+    print(node2.__repr__())
+    print(node1.__eq__(node2))
+
+
+main()
---
+++ src/textnode.py
@@ -0,0 +1,17 @@
+class TextNode:
+    def __init__(self, text, text_type, url=None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, t1):
+        if (
+                t1.text == self.text
+                and t1.text_type == self.text_type
+                and t1.url == self.url
+        ):
+            return True
+        return False
+
+    def __repr__(self) -> str:
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
