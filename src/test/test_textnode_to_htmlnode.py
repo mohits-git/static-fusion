7508038c6e100bd3e177b143afff4c0909deaab1
mohits-git <mohitkhatri2805@gmail.com>
2024-07-19 20:25:39 +0530

    feat: add tests for the textnode to htmlnode implementation

---
+++ src/test/test_textnode_to_htmlnode.py
@@ -0,0 +1,51 @@
+import unittest
+from app.leafnode import LeafNode
+from app.textnode_to_htmlnode import text_node_to_html_node
+from app.textnode import TextNode
+
+
+class TestTextNodeToHTMLNode(unittest.TestCase):
+    def test_text(self):
+        textnode = TextNode('hello mofos', 'text')
+        htmlnode = text_node_to_html_node(textnode)
+        expected_htmlnode = LeafNode('hello mofos')
+        self.assertEqual(htmlnode, expected_htmlnode)
+
+    def test_bold(self):
+        textnode = TextNode('hello mofos', 'bold')
+        htmlnode = text_node_to_html_node(textnode)
+        expected_htmlnode = LeafNode('hello mofos', 'b')
+        self.assertEqual(htmlnode, expected_htmlnode)
+
+    def test_italic(self):
+        textnode = TextNode('hello mofos', 'italic')
+        htmlnode = text_node_to_html_node(textnode)
+        expected_htmlnode = LeafNode('hello mofos', 'i')
+        self.assertEqual(htmlnode, expected_htmlnode)
+
+    def test_code(self):
+        textnode = TextNode('hello mofos', 'code')
+        htmlnode = text_node_to_html_node(textnode)
+        expected_htmlnode = LeafNode('hello mofos', 'code')
+        self.assertEqual(htmlnode, expected_htmlnode)
+
+    def test_link(self):
+        textnode = TextNode('hello mofos', 'link', 'http://localhost:3000')
+        htmlnode = text_node_to_html_node(textnode)
+        expected_htmlnode = LeafNode('hello mofos', 'a', {
+            "href": 'http://localhost:3000'
+        })
+        self.assertEqual(htmlnode, expected_htmlnode)
+
+    def test_image(self):
+        textnode = TextNode(None, 'image', 'public/dog.png')
+        htmlnode = text_node_to_html_node(textnode)
+        expected_htmlnode = LeafNode(None, 'img', {
+            'src': 'public/dog.png',
+            'alt': 'image',
+        })
+        self.assertEqual(htmlnode, expected_htmlnode)
+
+
+if __name__ == "__main__":
+    unittest.main()
