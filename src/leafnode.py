f75fc74a12e747538b928a10eb58583ff9514545
mohits-git <mohitkhatri2805@gmail.com>
2024-07-19 13:18:49 +0530

    feat: add LeafNode for plaintext or only text content elements

---
+++ src/leafnode.py
@@ -0,0 +1,19 @@
+from htmlnode import HTMLNode
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, value, tag=None, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.tag is None:
+            return self.value
+        html_node_str = ""
+        if self.tag is not None:
+            html_node_str += f"<{self.tag}"
+        html_node_str += self.props_to_html()
+        html_node_str += ">"
+        html_node_str += str(self.value)
+        if self.tag is not None:
+            html_node_str += f"<{'/'}{self.tag}>"
+        return html_node_str
