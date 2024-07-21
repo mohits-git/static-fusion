3bd428c9cf824fd7846aa3307d86472b5145dff5
mohits-git <mohitkhatri2805@gmail.com>
2024-07-21 19:38:26 +0530

    feat: add Markdown single text line to TextNodes conversion implementation

---
+++ src/app/text_to_textnodes.py
@@ -0,0 +1,29 @@
+from app.textnode import TextNode
+from app.text_type import text_type
+from app.split_textnodes import (
+    split_nodes_delimiter,
+    split_nodes_image,
+    split_nodes_link
+)
+
+
+def text_to_textnodes(text):
+    initial_textnodes = [TextNode(text, text_type.text.name)]
+    split_bold_nodes = split_nodes_delimiter(
+        initial_textnodes,
+        '**',
+        text_type.bold.name
+    )
+    split_italic_nodes = split_nodes_delimiter(
+        split_bold_nodes,
+        '*',
+        text_type.italic.name
+    )
+    split_code_nodes = split_nodes_delimiter(
+        split_italic_nodes,
+        '`',
+        text_type.code.name
+    )
+    split_link_nodes = split_nodes_link(split_code_nodes)
+    split_image_nodes = split_nodes_image(split_link_nodes)
+    return split_image_nodes
