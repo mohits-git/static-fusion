a45efe83f9632eb589667a242cad1d67fb740f7b
mohits-git <mohitkhatri2805@gmail.com>
2024-07-22 19:08:39 +0530

    feat: add start up code to generate html page from the demo content/index.md markdown

--- src/main.py
+++ src/main.py
@@ -1,8 +1,15 @@
+import os
 from app.copy_static import copy_static
+from app.generate_page import generate_page
 
 
 def main():
     copy_static()
+    cwd = os.getcwd()
+    from_path = os.path.join(cwd, 'content/index.md')
+    template_path = os.path.join(cwd, 'template.html')
+    dest_path = os.path.join(cwd, 'public/index.html')
+    generate_page(from_path, template_path, dest_path)
 
 
 main()
