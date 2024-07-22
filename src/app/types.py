0cdf82ce3617bc9000c5b2f8596e7d066c9ab21f
mohits-git <mohitkhatri2805@gmail.com>
2024-07-22 09:03:57 +0530

    feat: add block_type enum

--- src/app/types.py
+++ src/app/types.py
@@ -5,3 +5,8 @@ text_type = Enum(
     'text_type',
     ['text', 'bold', 'italic', 'code', 'link', 'image']
 )
+
+block_type = Enum(
+    'block_type',
+    ['paragraph', 'heading', 'code', 'quote', 'unordered_list', 'ordered_list']
+)
